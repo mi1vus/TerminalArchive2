4a45e10fa994db1defee35d7fc65f5a42d4551be
Max <milvus@e1.ru>
2018-09-04 15:02:48 +0500

    terminal filter

--- TerminalArchive.WebUI/Controllers/TerminalMonitoringController.cs
+++ TerminalArchive.WebUI/Controllers/TerminalMonitoringController.cs
@@ -13,10 +13,11 @@ namespace TerminalArchive.WebUI.Controllers
     {
         private readonly ITerminalRepository _repository;
         public int PageSize = 10;
+        public static List<string> TerminalNames = new List<string>();
 
         public TerminalMonitoringController()
         {
-            _repository = new TerminalRepository {UserName = User?.Identity?.Name };
+            _repository = new TerminalRepository { UserName = User?.Identity?.Name };
         }
 
         public TerminalMonitoringController(ITerminalRepository repo)
@@ -51,8 +52,9 @@ namespace TerminalArchive.WebUI.Controllers
 
             int maxPages;
             var terminals = _repository.Terminals as IList<Terminal> ?? _repository.Terminals.ToList();
+            TerminalNames = terminals.Select(t => t.Name).ToList();
             bool filter = FilterTerminalGroup > 0 || !string.IsNullOrWhiteSpace(FilterTerminalName);
-            Func<Terminal, bool> PredicatGroup = t=>true;
+            Func<Terminal, bool> PredicatGroup = t => true;
             if (FilterTerminalGroup > 0)
                 PredicatGroup = t => t.IdGroup == FilterTerminalGroup;
 
@@ -69,12 +71,12 @@ namespace TerminalArchive.WebUI.Controllers
                 maxPages = (int)Math.Ceiling((decimal)totalItems / PageSize);
 
             terminalsModel.Terminals =
-            from terminal in terminals.Where(t => PredicatGroup(t) && PredicatName(t)).OrderBy(t => t.Name).Skip((page - 1)*PageSize).Take(PageSize)
-                select new ViewTerminal(terminal)
-                {
-                    GroupsIdsString = terminal.IdGroup > 0 ? terminal.IdGroup.ToString() : " - ",
-                    GroupsNamesString = terminal.Group?.Name ?? " - "
-                };
+            from terminal in terminals.Where(t => PredicatGroup(t) && PredicatName(t)).OrderBy(t => t.Name).Skip((page - 1) * PageSize).Take(PageSize)
+            select new ViewTerminal(terminal)
+            {
+                GroupsIdsString = terminal.IdGroup > 0 ? terminal.IdGroup.ToString() : " - ",
+                GroupsNamesString = terminal.Group?.Name ?? " - "
+            };
             terminalsModel.PagingInfo = new PagingInfo
             {
                 CurrentPage = page > maxPages ? maxPages : page,
@@ -84,6 +86,33 @@ namespace TerminalArchive.WebUI.Controllers
 
             ViewBag.CurrentController = GetType().ToString();
             return View(terminalsModel);
+        }
+        //[System.Web.Services.WebMethodAttribute(), System.Web.Script.Services.ScriptMethodAttribute()]
+        //public static string[] GetCompletionList(string prefixText, int count, string contextKey)
+        //{
+        //    // Create array of movies  
+        //    string[] movies = { "Star Wars", "Star Trek", "Superman", "Memento", "Shrek", "Shrek II" };
+
+        //    // Return matching movies  
+        //    return (from m in movies where m.StartsWith(prefixText, StringComparison.CurrentCultureIgnoreCase) select m).Take(count).ToArray();
+        //}
+
+
+        public JsonResult GetListForAutoComplite(string prefixText, int maxRows)
+        {
+            // Create array of movies  
+            //string[] movies = { "Star Wars", "Star Trek", "Superman", "Memento", "Shrek", "Shrek II" };
+
+            // Return matching movies  
+            return Json(from m in TerminalNames where m.StartsWith(prefixText, StringComparison.CurrentCultureIgnoreCase) select m);
+
+
+            //var company = Json(from t in context.Company
+            //                   where t.Name.ToUpper().Contains(q.ToUpper())
+            //                   select new { t.CompanyID, t.Name });
+
+
+            //return company;
         }
     }
 }
